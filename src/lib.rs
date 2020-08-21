ba6842a9081b3c5d0d2eb546b5e5d9b78debf826
Antony Searle <antony.searle@gmail.com>
2020-08-22 09:44:57 +1000

    Option

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use std::cmp;
 use std::fmt;
 use std::marker::PhantomData;
 use std::mem;
+use std::option::Option;
 use std::sync::{Arc, atomic::{Ordering, AtomicPtr}};
 use crossbeam::epoch;
 
@@ -13,14 +14,17 @@ pub trait Pointer<T> {
     unsafe fn from_ptr(ptr: *const T) -> Self;
 }
 
-impl<T> Pointer<T> for Arc<T> {
+impl<T> Pointer<T> for Option<Arc<T>> {
 
     fn into_ptr(self) -> *const T {
-        Arc::into_raw(self)
+        match self {
+            Some(arc) => Arc::into_raw(arc),
+            None => std::ptr::null(),
+        }
     }
 
     unsafe fn from_ptr(ptr: *const T) -> Self {
-        Arc::from_raw(ptr)
+        ptr.as_ref().map(|ptr| { Arc::from_raw(ptr) })
     }
 
 }
@@ -59,41 +63,38 @@ impl <'g, T> Pointer<T> for Shared<'g, T> {
 
 impl<'g, T> Shared<'g, T> {
 
-    pub fn from_arc(arc: Arc<T>) -> Self {
+    pub fn from_arc(arc: Option<Arc<T>>) -> Self {
         Self {
-            ptr: Arc::into_raw(arc),
+            ptr: arc.map_or(std::ptr::null(), |arc| { Arc::into_raw(arc) }),
             _marker: PhantomData,
         }
     }
 
-    pub unsafe fn into_arc(self) -> Arc<T> {
-        Arc::from_raw(self.ptr)
+    pub unsafe fn as_arc(&self) -> Option<Arc<T>> {
+        self.ptr.as_ref().map(|ptr| { Arc::from_raw(ptr) })
+    }
+
+    pub unsafe fn into_arc(self) -> Option<Arc<T>> {
+        self.as_arc()
     }
 
     pub fn as_ptr(&self) -> *const T {
         self.ptr
     }
 
-    pub fn deref(&self) -> &T {
-        unsafe {
-            &*self.ptr
-        }
+    pub unsafe fn as_ref(&self) -> Option<&T> {        
+        self.ptr.as_ref()
     }
 
     pub fn increment(&self) -> &Self {
-        let a = unsafe { Arc::from_raw(self.ptr) };
-        mem::forget(a.clone());
-        mem::forget(a);
+        let tmp = unsafe { self.as_arc() };
+        mem::forget(tmp.clone());
+        mem::forget(tmp);
         self
     }
 
-    pub fn increment_and_into_arc(self) -> Arc<T> {
-        let a = unsafe { Self::into_arc(self) };
-        mem::forget(a.clone());
-        a
-    }
-
     pub unsafe fn deferred_decrement(&self, guard: &'g epoch::Guard) -> &Self {
+        assert!(!self.ptr.is_null());
         let p : *const T = self.ptr;
         guard.defer_unchecked(move || { Arc::from_raw(p) });
         self
@@ -139,11 +140,11 @@ impl<T: Send + Sync> Atomic<T> {
     /// use std::sync::atomic::Ordering;
     /// use crossbeam::epoch;
     ///
-    /// let atomic = carc::Atomic::new(Arc::new(1234));
+    /// let atomic = carc::Atomic::new(Some(Arc::new(1234)));
     /// let guard = epoch::pin();
     /// let shared = atomic.load(Ordering::Acquire, &guard);
     /// unsafe { 
-    ///     assert_eq!(shared.deref(), &1234);
+    ///     assert_eq!(shared.as_ref().unwrap(), &1234);
     /// }
     /// ```
     pub fn load<'g>(&self, ord: Ordering, _: &'g epoch::Guard) -> Shared<'g, T> {
@@ -159,12 +160,12 @@ impl<T: Send + Sync> Atomic<T> {
     /// use std::sync::atomic::Ordering;
     /// use crossbeam::epoch;
     /// 
-    /// let atomic = carc::Atomic::new(Arc::new(1234));
+    /// let atomic = carc::Atomic::new(Some(Arc::new(1234)));
     /// let guard = epoch::pin();
-    /// let old = atomic.swap(Arc::new(5678), Ordering::AcqRel, &guard);
+    /// let old = atomic.swap(Some(Arc::new(5678)), Ordering::AcqRel, &guard);
     /// unsafe { 
     ///     old.deferred_decrement(&guard); // drop old value eventually
-    ///     assert_eq!(old.deref(), &1234); // still valid for now
+    ///     assert_eq!(old.as_ref().unwrap(), &1234); // still valid for now
     /// }
     /// ```
     pub fn swap<'g, P: Pointer<T>>(&self, new: P, ord: Ordering, _: &'g epoch::Guard) -> Shared<'g, T> {
@@ -182,9 +183,9 @@ impl<T: Send + Sync> Atomic<T> {
     /// use std::sync::atomic::Ordering;
     /// use crossbeam::epoch;
     /// 
-    /// let atomic = carc::Atomic::new(Arc::new(1234));
+    /// let atomic = carc::Atomic::new(Some(Arc::new(1234)));
     /// let guard = epoch::pin();
-    /// atomic.store(Arc::new(5678), Ordering::Release, &guard); 
+    /// atomic.store(Some(Arc::new(5678)), Ordering::Release, &guard); 
     /// // old value is lost and leaked - prefer swap
     /// ```
     pub fn store<'g, P: Pointer<T>>(&self, new: P, ord: Ordering, _: &'g epoch::Guard) {
@@ -199,17 +200,17 @@ impl<T: Send + Sync> Atomic<T> {
     /// use std::sync::{Arc, atomic::Ordering};
     /// use crossbeam::epoch;
     /// 
-    /// let a = carc::Atomic::new(Arc::new(1234));
+    /// let a = carc::Atomic::new(Some(Arc::new(1234)));
     /// let guard = epoch::pin();
     /// let old = a.load(Ordering::Acquire, &guard);
-    /// let new = carc::Shared::from_arc(Arc::new(1 + unsafe { *old.deref() }));
-    /// unsafe { 
+    ///  unsafe {
+    ///     let new = carc::Shared::from_arc(Some(Arc::new(1 + old.as_ref().unwrap())));
     ///     match a.compare_exchange(old, new, Ordering::AcqRel, Ordering::Relaxed, &guard) {
     ///         Ok(old) => old,
     ///         Err(old) => new
     ///     }.deferred_decrement(&guard); // clean up what wasn't installed
-    ///     let new = a.load(Ordering::Acquire, &guard).increment_and_into_arc();
-    ///     assert_eq!(*new, 1235);
+    ///     let new = a.load(Ordering::Acquire, &guard).increment().as_arc();
+    ///     assert_eq!(new.unwrap().as_ref(), &1235);
     /// }
     /// ```
     pub fn compare_exchange<'g, P: Pointer<T>, Q: Pointer<T>>(&self, current: P, new: Q, success: Ordering, failure: Ordering, _guard: &'g epoch::Guard) -> Result<Shared<T>, P>{        
